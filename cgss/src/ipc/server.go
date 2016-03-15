bc950ac6316f1188ec363187bdf2e393451e617e
wzjg520 <wzjg520@126.com>
2016-03-15 18:14:00 +0800

    init

---
+++ cgss/src/ipc/server.go
@@ -0,0 +1,58 @@
+package ipc
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Request struct {
+	Method string `json:"method"`
+	Params string `json:"params"`
+}
+
+type Response struct {
+	Code string `json:"code"`
+	Body string `json:"body"`
+}
+
+type Server interface {
+	Name() string
+	Handle(method, params string) *Response
+}
+
+type Ipcserver struct {
+	Server
+}
+
+func NewIpcserver(server Server) *Ipcserver {
+	return &Ipcserver{server}
+}
+
+func (server *Ipcserver)Connect() chan string {
+	session := make(chan string, 0)
+
+	go func(c chan string) {
+		for {
+			request := <-c
+			if request == "CLOSE" {
+				break
+			}
+
+			var req Request
+			err := json.Unmarshal([]byte(request), &req)
+
+			if err != nil {
+				fmt.Println("Invalid request format:", request)
+				return
+			}
+
+			resp := server.Handle(req.Method, req.Params)
+
+			b, err := json.Marshal(resp)
+			c <- string(b)
+		}
+		fmt.Println("Session closed.")
+	}(session)
+	fmt.Println("A new session has been created successfully.")
+	return session
+}
