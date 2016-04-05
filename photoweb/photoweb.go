2ed56443d1584435b7979e934e0aa387ae8f7033
wzjg520 <wzjg520@126.com>
2016-04-05 22:37:14 +0800

    upload

--- photoweb/photoweb.go
+++ photoweb/photoweb.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"io/ioutil"
 )
 
 const (
@@ -31,10 +32,21 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		filename := h.Filename
+		defer f.Close()
+
+		_, err = os.Stat(UPLOAD_DIR)
+
+		if err != nil && os.IsNotExist(err) {
+			errDir := os.Mkdir(UPLOAD_DIR, 0777)
+			if errDir != nil {
+				http.Error(w, errDir.Error(), http.StatusInternalServerError)
+			}
+		}
+
 		t, err := os.Create(UPLOAD_DIR + "/" + filename)
 
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError+2)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		defer t.Close()
 		if _, err := io.Copy(t, f); err != nil {
@@ -45,9 +57,50 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	if exists := isExists(imagePath); !exists {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-type", "image")
+	http.ServeFile(w, r, imagePath)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir(UPLOAD_DIR)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var listHtml string
+	for _, fileInfo := range fileInfoArr {
+		imgid := fileInfo.Name()
+		listHtml += "<li><a target=\"_blank\" href=\"/view?id="+ imgid +"\">" + imgid + "</a></li>"
+	}
+	w.Header().Set("Content-type", "text/html")
+	io.WriteString(w, "<ol>" + listHtml + "</ol>")
+
+}
+
+func isExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
+}
+
+
+
 func main() {
 
 	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/view", viewHandler)
+	http.HandleFunc("/", listHandler)
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
